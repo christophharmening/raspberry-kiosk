f1f390bd0498576e00554ac4bd31b1e42c6b0b2b
christophharmening <52511276+christophharmening@users.noreply.github.com>
2023-05-10 15:13:27 +0200

    Update autostart.sh

--- template/home/user/.config/openbox/autostart.sh
+++ template/home/user/.config/openbox/autostart.sh
@@ -6,7 +6,7 @@ xset -dpms
 
 unclutter &
 
-chromium --noerrdialogs --disable-infobars --kiosk -URL- &
+chromium-browser --noerrdialogs --disable-infobars --kiosk -URL- &
 
 while true ; do
 	sleep -TIME-
