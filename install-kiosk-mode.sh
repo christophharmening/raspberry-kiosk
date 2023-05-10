bef14e5cf5b64c5147b8055b1eed7e484a864298
christophharmening <52511276+christophharmening@users.noreply.github.com>
2023-05-10 12:37:29 +0200

    Update install-kiosk-mode.sh

--- install-kiosk-mode.sh
+++ install-kiosk-mode.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-# First upgrade system
-apt update && apt upgrade -y
+# root ?
+if [ "$EUID" -ne 0 ] ; then
+	echo "Please run as root!"
+	exit 1
+fi
 
+# Variablen
+username=pi
+file_konsole_autologin="/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+
+
+# First upgrade system
+apt-get update && apt-egt upgrade -y
+
+# Needed GUI tools
+apt-get install -y openbox lightdm chromium-browser xdotool unclutter
+
+# GUI autologin
+systemctl set-default graphical.target
+if [ -f ${file_konsole_autologin} ] ; then rm -rf ${file_konsole_autologin} ; fi
+ sedtc/lightdm/lightdm.conf -i -e "s/^autologin-user=.*/#autologin-user=/"
+ sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=/autologin-user=${username}"
+ 
